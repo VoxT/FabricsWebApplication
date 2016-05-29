fa2f3b053dc47d9a3f99e88743eeb9452bde365b
Vo <votienthieu@gmail.com>
2016-05-29 21:19:12 +0700

    bootstrap to show

--- FabricsWebApplication/Controllers/ShowController.cs
+++ FabricsWebApplication/Controllers/ShowController.cs
@@ -13,7 +13,7 @@ namespace FabricsWebApplication.Controllers
 {
     public class ShowController : Controller
     {
-        
+
         public ActionResult View1()
         {
             return View();
@@ -37,7 +37,11 @@ namespace FabricsWebApplication.Controllers
             List<Supplier> model = new List<Supplier>();
 
             model = supplier.GetAll();
-           
+            foreach (var sup in model)
+            {
+                @ViewData[sup.Id.ToString()] = supplier.GetDebt(sup.Id.ToString());
+            }
+
             return View(model);
         }
 
@@ -63,6 +67,55 @@ namespace FabricsWebApplication.Controllers
             return View(model);
         }
 
+        public ActionResult ViewPurchaseOrder()
+        {
+            PurchaseOrderService purchaseOrder = new PurchaseOrderService();
+
+            List<PurchaseOrder> model = new List<PurchaseOrder>();
+
+            model = purchaseOrder.GetAll();
+
+            return View(model);
+        }
+
+        public ActionResult ViewPurchaseInvoice()
+        {
+            PurchaseInvoiceService purchaseInvoice = new PurchaseInvoiceService();
+
+            List<PurchaseInvoice> model = new List<PurchaseInvoice>();
+
+            model = purchaseInvoice.GetAll();
+            List<Double> listTotalMoney = new List<Double>();
+            foreach (var ex in model)
+            {
+                @ViewData[ex.SupplierId.ToString()] = purchaseInvoice.GetTotalMoney(ex);
+            }
+
+            return View(model);
+        }
+
+        public ActionResult ViewPayment()
+        {
+            PaymentService payment = new PaymentService();
+
+            List<Payment> model = new List<Payment>();
+
+            model = payment.GetAll();
+
+            return View(model);
+        }
+
+        public ActionResult ViewRecord()
+        {
+            RecordService record = new RecordService();
+
+            List<Record> model = new List<Record>();
+
+            model = record.GetAll();
+
+            return View(model);
+        }
+
         public ActionResult ViewFabrics()
         {
             FabricsService fabrics = new FabricsService();
@@ -73,6 +126,50 @@ namespace FabricsWebApplication.Controllers
 
             return View(model);
         }
-        
+
+        public ActionResult ViewFabricsColor()
+        {
+            FabricsColorService fabricsColor = new FabricsColorService();
+
+            List<FabricsColor> model = new List<FabricsColor>();
+
+            model = fabricsColor.GetAll();
+
+            return View(model);
+        }
+
+        public ActionResult ViewFabricsRoll()
+        {
+            FabricsRollService fabricsRoll = new FabricsRollService();
+
+            List<FabricsRoll> model = new List<FabricsRoll>();
+
+            model = fabricsRoll.GetAll();
+
+            return View(model);
+        }
+
+        public ActionResult ViewFabricsPlots()
+        {
+            FabricsPlotsService fabricsPlots = new FabricsPlotsService();
+
+            List<FabricsPlots> model = new List<FabricsPlots>();
+
+            model = fabricsPlots.GetAll();
+
+            return View(model);
+        }
+
+        public ActionResult ViewWareHouse()
+        {
+            WareHouseService wareHouse = new WareHouseService();
+
+            List<WareHouse> model = new List<WareHouse>();
+
+            model = wareHouse.GetAll();
+
+            return View(model);
+        }
+
     }
 }
